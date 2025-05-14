8c22004b99edfe84faf94f1252cf1f5c3b606e2e
pc <pc@DESKTOP-VMM42G7>
2025-05-13 21:23:13 -0500

    Index in PHP that conect the services

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hello Front PHP
+
+This project serves a simple HTML interface using PHP built-in server. for conect diferent PAAS services
+
+## Run Locally with Docker
+
+```bash
+docker build -t hello-front-php .
+docker run -p 8000:8000 hello-front-php
+```
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 50px;
+        }
+        button {
+            margin: 10px;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <h1>Index From PAAS services</h1>
+
+    <button onclick="window.location.href='https://sumador-python-petter-07ab67f9b263.herokuapp.com/'">Heroku</button>
+    <button onclick="window.location.href='https://www.digitalocean.com'">Digital Ocean</button>
+    <button onclick="window.location.href='https://hello-world-python-production-9769.up.railway.app/'">Railway</button>
+    <button onclick="window.location.href='https://vercel.com'">Vercel</button>
+    <button onclick="window.location.href='https://www.google.com'">Google</button>
+</body>
+</html>
