11feeb34038842d270dbe8430a0be56a77508237
Kevin Eduardo Anchali Zumba <tevineduardo@gmail.com>
2025-05-16 13:11:00 -0500

    change the links

--- index.php
+++ index.php
@@ -22,9 +22,9 @@
     <h1>Index From PAAS services</h1>
 
     <button onclick="window.location.href='https://sumador-python-petter-07ab67f9b263.herokuapp.com/'">Heroku</button>
-    <button onclick="window.location.href='https://www.digitalocean.com'">Digital Ocean</button>
+    <button onclick="window.location.href='https://whale-app-y259c.ondigitalocean.app/'">Digital Ocean</button>
     <button onclick="window.location.href='https://hello-world-python-production-9769.up.railway.app/'">Railway</button>
-    <button onclick="window.location.href='https://repos1-86ry.vercel.app/'">Vercel</button>
+    <button onclick="window.location.href='https://repos1-sable.vercel.app/'">Vercel</button>
     <button onclick="window.location.href='https://www.google.com'">Google</button>
 </body>
 </html>
